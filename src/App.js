0681ab7ad658e0fd19a309613283d7c07ccccfbc
Edson Gabriel <107326349+gabrielmjacques@users.noreply.github.com>
2023-02-08 16:43:34 -0300

    Funções dos botões terminadas

--- src/App.js
+++ src/App.js
@@ -5,49 +5,113 @@ import './App.css';
 function App()
 {
   // States
-  const [ screenValue, setScreenValue ] = useState( 0 )
 
+  const [ screenValue, setScreenValue ] = useState( '' )
+
+  // Vars
+
+  /* Verificação se o último char não é um número */
+  const checkLastChar = screenValue[ screenValue.length - 1 ] == '+' || screenValue[ screenValue.length - 1 ] == '-' || screenValue[ screenValue.length - 1 ] == 'X' || screenValue[ screenValue.length - 1 ] == '÷' || screenValue[ screenValue.length - 1 ] == '.'
 
   // Functions
+
+  /* Adicionar valor */
   const addValue = ( v ) =>
   {
-    if ( screenValue == '0' || screenValue == undefined )
+    if ( screenValue == '' && ( v != 'X' && v != '÷' && v != '.' ) )
     {
-      setScreenValue()
-      setScreenValue( v )
+      setScreenValue( screenValue + v )
 
-    } else
+    } else if ( screenValue != '' )
     {
-      setScreenValue( screenValue + '' + v )
+      if ( checkLastChar && ( v == '+' || v == '-' || v == 'X' || v == '÷' || v == '.' ) )
+      {
+
+      } else
+      {
+        setScreenValue( screenValue + v )
+      }
     }
   }
 
+  /* Adicionar valor através das teclas */
+  const addValueKeyPress = ( e ) =>
+  {
+    let keyValue = e.key
+
+    const numbers = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+    const operations = [ '+', '-', '*', '/' ]
+
+    if ( numbers.indexOf( keyValue ) >= 0 )
+    {
+      addValue( keyValue )
+    }
+
+    if ( operations.indexOf( keyValue ) >= 0 )
+    {
+      if ( keyValue == '*' )
+      {
+        addValue( 'X' )
+      } else if ( keyValue == '/' )
+      {
+        addValue( '÷' )
+      } else
+      {
+        addValue( keyValue )
+      }
+    }
+
+    if ( keyValue == 'c' )
+    {
+      clean()
+    }
+
+    if ( keyValue == 'Backspace' )
+    {
+      backspace()
+    }
+
+    if ( keyValue == 'Enter' )
+    {
+      resultValue()
+    }
+  }
+
+  /* Apagar */
   const backspace = () =>
   {
     setScreenValue( screenValue.slice( 0, -1 ) )
   }
 
+  /* Apagar tudo */
   const clean = () =>
   {
-    setScreenValue( '0' )
+    setScreenValue( '' )
   }
 
+  /* Resultado */
   const resultValue = () =>
   {
-    if ( screenValue == '' )
+    if ( screenValue != '' && screenValue.length > 1 )
     {
-      setScreenValue( '0' )
+      let result = screenValue
+
+      if ( checkLastChar )
+      {
+        result = result.slice( 0, -1 )
+      }
+
+      result = result.replaceAll( 'X', '*' ).replaceAll( '÷', '/' )
+      result = eval( result )
+
+      setScreenValue( String( result ) )
     }
-
-    let result = eval( screenValue.replaceAll( 'X', '*' ).replaceAll( '÷', '/' ) )
-
-    setScreenValue( result.toString() )
   }
 
 
 
   return (
-    <div className="App">
+    <div onKeyDown={ ( e ) => addValueKeyPress( e ) } className="App">
       <div className='Calculator'>
 
         <Screen screenValue={ screenValue } />
@@ -56,31 +120,30 @@ function App()
 
           <button onClick={ () => clean() }>C</button>
           <button onClick={ () => backspace() }>&larr;</button>
-          <button onClick={ () => alert( 'Indisponível' ) }>%</button>
-          <button onClick={ () => addValue( '÷' ) }>÷</button>
+          <button className='Ops' onClick={ () => addValue( '÷' ) }>÷</button>
+          <button></button>
 
           <button onClick={ () => addValue( '7' ) }>7</button>
           <button onClick={ () => addValue( '8' ) }>8</button>
           <button onClick={ () => addValue( '9' ) }>9</button>
 
-          <button onClick={ () => addValue( 'X' ) }>X</button>
+          <button className='Ops' onClick={ () => addValue( 'X' ) }>X</button>
 
           <button onClick={ () => addValue( '4' ) }>4</button>
           <button onClick={ () => addValue( '5' ) }>5</button>
           <button onClick={ () => addValue( '6' ) }>6</button>
 
-          <button onClick={ () => addValue( '-' ) }>-</button>
+          <button className='Ops' onClick={ () => addValue( '-' ) }>-</button>
 
           <button onClick={ () => addValue( '1' ) }>1</button>
           <button onClick={ () => addValue( '2' ) }>2</button>
           <button onClick={ () => addValue( '3' ) }>3</button>
 
-          <button onClick={ () => addValue( '+' ) }>+</button>
-          <button onClick={ () => alert( 'Indisponível' ) }>+/-</button>
+          <button className='Ops' onClick={ () => addValue( '+' ) }>+</button>
           <button onClick={ () => addValue( '0' ) }>0</button>
-          <button onClick={ () => alert( 'Indisponível' ) }>.</button>
+          <button onClick={ () => addValue( '.' ) }>.</button>
 
-          <button onClick={ () => resultValue() }>=</button>
+          <button className='IgualsButton' onClick={ () => resultValue() }>=</button>
 
         </div>
 
